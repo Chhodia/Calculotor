ec4c846ba669651f6c962952d92d291f74db8c65
Chhodia <97937732+Chhodia@users.noreply.github.com>
2023-03-28 08:48:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const plus = document.querySelector('.js-plus');
 const inpit = document.querySelector('.js-inpit')
 
-console.log(123)
+const Add = (number1, number2) => {
+    return number1 + number2
+}
+const Minus = (number1, number2) => {
+    return number1 - number2
+}
+const Multply = (number1, number2) => {
+    return number1 * number2
+}
+const Divide = (number1, number2) => {
+    return number1 / number2
+}
+plus.addEventListener("click", () => {
+    function Add() {
+        
+    }
+})
